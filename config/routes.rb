2fffcfab2a8323177c67abf345140db7b6a44c76
EC2 Default User <ec2-user@ip-172-31-82-255.ec2.internal>
2019-11-01 02:39:15 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources 'top'
+  
+  resources "users", only: [:show, :edit, :update]
+root "top#index"
+  resources :groups, only: [:show, :edit, :update]
+  resources :questions, only: [:create, :show]
+  resources :answers, only: [:new, :create, :edit, :update]
+  
+end
